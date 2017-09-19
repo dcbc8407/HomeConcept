262cd024f7538518e35299f935c853bdec85ece0
dcbc8407 <Derek.A.Caldwell@gmail.com>
2017-09-18 21:38:16 -0500

    home calculate attempt

--- HomeConcept/SavingGrace/QuoteBuilder.cs
+++ HomeConcept/SavingGrace/QuoteBuilder.cs
@@ -15,11 +15,43 @@ namespace SavingGrace
         public QuoteBuilder()
         {
             InitializeComponent();
+            textBox1.Text = "1";
+            label4.Text = "1";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            //Closes QuoteBuilder Window
             Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem=="Shoes")
+            {
+                label4.Text = "$35.00";
+            }
+            else if(comboBox1.SelectedItem=="Socks")
+            {
+                label4.Text = "$7.00";
+            }
+            else
+            {
+                label4.Text = null;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            /*int toInteger;
+            int toInteger2;
+            int sum;
+            toInteger = int.Parse(textBox1.Text);
+            toInteger2= int.Parse(label4.Text);
+            sum = toInteger * toInteger2;
+            label6.Text = sum.ToString();*/
+
+
         }
     }
 }
